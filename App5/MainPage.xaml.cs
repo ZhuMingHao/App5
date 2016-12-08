abd9f412de12fe900e06087a6cdb8a7527e1c2a9
v-zhumin <v-zhumin@MSDNUWPSERVER1>
2016-12-08 18:26:24 +0800

    Add project files.

---
+++ App5/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Sensors;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App5
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private OrientationSensor _sensor;
+
+        private async void ReadingChanged(object sender, OrientationSensorReadingChangedEventArgs e)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                OrientationSensorReading reading = e.Reading;
+
+                // Quaternion values
+                txtQuaternionX.Text = String.Format("{0,8:0.00000}", reading.Quaternion.X);
+                txtQuaternionY.Text = String.Format("{0,8:0.00000}", reading.Quaternion.Y);
+                txtQuaternionZ.Text = String.Format("{0,8:0.00000}", reading.Quaternion.Z);
+                txtQuaternionW.Text = String.Format("{0,8:0.00000}", reading.Quaternion.W);
+
+                // Rotation Matrix values
+                txtM11.Text = String.Format("{0,8:0.00000}", reading.RotationMatrix.M11);
+                txtM12.Text = String.Format("{0,8:0.00000}", reading.RotationMatrix.M12);
+                txtM13.Text = String.Format("{0,8:0.00000}", reading.RotationMatrix.M13);
+                txtM21.Text = String.Format("{0,8:0.00000}", reading.RotationMatrix.M21);
+                txtM22.Text = String.Format("{0,8:0.00000}", reading.RotationMatrix.M22);
+                txtM23.Text = String.Format("{0,8:0.00000}", reading.RotationMatrix.M23);
+                txtM31.Text = String.Format("{0,8:0.00000}", reading.RotationMatrix.M31);
+                txtM32.Text = String.Format("{0,8:0.00000}", reading.RotationMatrix.M32);
+                txtM33.Text = String.Format("{0,8:0.00000}", reading.RotationMatrix.M33);
+            });
+        }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            _sensor = OrientationSensor.GetDefault();
+
+            // Establish the report interval for all scenarios
+            uint minReportInterval = _sensor.MinimumReportInterval;
+            uint reportInterval = minReportInterval > 16 ? minReportInterval : 16;
+            _sensor.ReportInterval = reportInterval;
+
+            // Establish event handler
+            _sensor.ReadingChanged += new TypedEventHandler<OrientationSensor, OrientationSensorReadingChangedEventArgs>(ReadingChanged);
+        }
+
+    }
+}
